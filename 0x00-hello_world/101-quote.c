de46bf0d668d40c6a4f3704dc8acb6a477861235
ziad923 <zwalid1919@gmail.com>
2023-03-16 03:54:18 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - entry poit
+*Return: always 1 (success)
+*/
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
