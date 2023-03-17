132326d0ae02e31a7d495bba2c514a91fc97f3e5
ziad923 <zwalid1919@gmail.com>
2023-03-17 08:39:45 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - entry poit
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= 9; ch++)
+	{
+		putchar(ch);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('$');
+	return (0);
+}
