02710d4360f96501075f93e159f634b91d0ac3cb
ziad923 <zwalid1919@gmail.com>
2023-03-17 11:42:55 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*main - entry poit
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= 8; a++)
+	{
+		for (b = '1'; b <= 9; b++)
+		{
+			if (b > a)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != 8 || b != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
