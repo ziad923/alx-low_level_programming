30b2b6c326571a82e27e079fe214122acbc1e953
ziad923 <zwalid1919@gmail.com>
2023-03-17 08:29:43 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - entry poit
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	int ch;
+	for (ch = '0'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
