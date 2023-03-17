7061d2ef1d66bbe1187aed165a4b025b1ff73db2
ziad923 <zwalid1919@gmail.com>
2023-03-17 08:24:28 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - entry poit
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
