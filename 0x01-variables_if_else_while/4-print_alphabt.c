f7e601b5fac8098156ca082dffd4d5c4b76ecd40
ziad923 <zwalid1919@gmail.com>
2023-03-17 07:56:55 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - entry poit
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' || ch != 'e')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
