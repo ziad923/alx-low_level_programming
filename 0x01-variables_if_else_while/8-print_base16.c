b5e6fc0dc221957368a1035253ee1ad76650b6ad
ziad923 <zwalid1919@gmail.com>
2023-03-17 08:32:24 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,7 +6,10 @@
 int main(void)
 {
 	int ch;
-	for (ch = '0'; ch <= 'f'; ch++)
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	for (ch = 'a'; ch <= 'f'; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
