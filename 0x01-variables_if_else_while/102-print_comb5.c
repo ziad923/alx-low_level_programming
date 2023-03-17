0817b0c85f8fd6c9fa97792ce418a84f05ae0c8c
ziad923 <zwalid1919@gmail.com>
2023-03-17 13:06:32 -0700

    Final task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,11 +13,11 @@ int main(void)
 		{
 			if (a < b)
 			{
-				putchar((a / 10) + '0');
-				putchar((a % 10) + '0');
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
 				putchar(' ');
-				putchar((b / 10) + '0');
-				putchar((b % 10) + '0');
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
 			if (a != 98 || b != 99)
 			{
 				putchar(',');
