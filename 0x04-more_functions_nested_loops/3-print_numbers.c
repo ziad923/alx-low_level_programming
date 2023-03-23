27d8837ad1ccb79c7c737b7d3d9fd64abcaba111
ziad923 <zwalid1919@gmail.com>
2023-03-23 05:01:59 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers - print 0-9
+* return: void
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
