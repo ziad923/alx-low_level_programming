ed772f9f64d8ce34e7a7f299afbd12cd0d5fdbf5
ziad923 <zwalid1919@gmail.com>
2023-03-23 13:14:57 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,21 @@ int main(void)
 	for (a = 1; a <= 100; a++)
 	{
 		if ((a % 3 == 0) && (a % 5 == 0))
+		{
 			printf("FizzBuzz ");
+		}
 		else if (a % 5 == 0)
+		{
 			printf("Buzz ");
+		}
 		else if (a % 3 == 0)
+		{
 			printf("Fizz ");
+		}
 		else
+		{
 			printf("%d ", a);
+		}
 	}
 	printf("\n");
 	return (0);
