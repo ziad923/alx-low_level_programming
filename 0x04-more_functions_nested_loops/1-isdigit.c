5dd2a414f0902ebddec832cad46334ee32533a51
ziad923 <zwalid1919@gmail.com>
2023-03-23 04:51:06 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isdigit - check if numbers are 0 - 9
+* @c: char to be checked
+*
+* Return: 0 or 1
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 58)
+		return (1);
+	else
+		return (0);
+}
