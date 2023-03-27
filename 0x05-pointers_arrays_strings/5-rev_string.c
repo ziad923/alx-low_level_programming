5ded62ba789248e12304a7933cbc93e7d6b283d2
ziad923 <zwalid1919@gmail.com>
2023-03-27 12:45:26 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* rev_string - Reverses a string
+* @s: Input string
+* Return: String in reverse
+*/
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	for (l = 0; s[l] != '\0'; ++l)
+		;
+	for (i = 0; i < 1 / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
+}
